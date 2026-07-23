09dfc5c6f2b472fc0dd2595c647fdd90d8374ce7
tsing kou ng <paulngyn512@gmail.com>
2026-07-23 11:34:03 -0700

    design update and dupe delete

--- mtc-saas-mobile/app/(auth)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-// app/(auth)/_layout.tsx – Auth group: no tab bar, simple stack
-
-import { Stack } from 'expo-router';
-
-export default function AuthLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }} />
-  );
-}
